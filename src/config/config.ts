a3b546c8ff8f55a92ec7f0b31add0d70c598f933
Hakizimana Fred <hakifred20@gmail.com>
2023-03-30 11:14:18 +0300

    cryptobot that automates trading on bybit exchange

---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+
+export const CONFIG = {
+  PORT: process.env.PORT || "",
+  API_KEY: process.env.BY_BIT_API_KEY || "",
+  API_SECRET: process.env.BY_BIT_API_SECRET || "",
+  TEST_NET: true,
+  URL: process.env.MAIN_NET_URL || ""
+};
---
+++ src/exchange/bybbit.ts
@@ -0,0 +1,58 @@
+import {
+  CoinParam,
+  LinearClient,
+  LinearOrder,
+  LinearPositionIdx,
+  NewLinearOrder,
+  SymbolParam,
+  WalletBalances,
+} from "bybit-api";
+import { CONFIG } from "../config/config";
+
+class ByBitExchange {
+  private linear: LinearClient;
+
+  constructor(params: { key: string; secret: string; testnet: boolean, url: string }) {
+    this.linear = new LinearClient(params);
+  }
+
+  async getOrderBook(params: SymbolParam): Promise<any[] | null> {
+    const order = await this.linear.getOrderBook({ symbol: params.symbol });
+    if (order) {
+      return [order];
+    }
+    return null;
+  }
+
+  async placeActiveOrder(params: {
+    symbol: string;
+    side: any;
+    qty: number;
+    order_type: any;
+    time_in_force: any;
+    reduce_only: boolean;
+    close_on_trigger: boolean;
+    price: number;
+  }): Promise<any | null> {
+    const order = await this.linear.placeActiveOrder(params);
+
+    console.log("Place order", order);
+
+    return null;
+  }
+
+  async getWalletBallance(params: CoinParam): Promise<WalletBalances | null> {
+    const myEth = await this.linear.getWalletBalance(params);
+
+    console.log(myEth);
+
+    return null;
+  }
+}
+
+export const bybit = new ByBitExchange({
+  key: CONFIG.API_KEY,
+  secret: CONFIG.API_SECRET,
+  testnet: CONFIG.TEST_NET,
+  url: CONFIG.URL
+});
